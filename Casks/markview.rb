951dd74111e594e23879cb734c61c4408d4b7091
Paul Kang <contact@paulkang.dev>
2026-08-23 16:30:34 +0000

    Update MarkView to 1.7.2

--- Casks/markview.rb
+++ Casks/markview.rb
@@ -1,6 +1,6 @@
 cask "markview" do
-  version "1.7.1"
-  sha256 "493fe41cb0ecd5b0ecd30d80e68a710b649f1c521739c48604753c67998cde6d"
+  version "1.7.2"
+  sha256 "0050f4416c190bf4d8773a2202ad905ae72ac7ad9ced397c08e6a507bc637706"
 
   url "https://github.com/paulhkang94/markview/releases/download/v#{version}/MarkView-#{version}.zip"
   name "MarkView"
